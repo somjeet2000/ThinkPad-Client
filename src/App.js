4c2e811d2c595788f54bf85f06b1a374fde05f5a
Somjeet Srimani <somjeetsrimani2000@gmail.com>
2024-07-06 02:43:29 +0530

    Created Navbar, Basic layout of Home, About components. Added the concurrently so that it can run both the servers at the same time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+
+/*
+Very Important:-
+1. In the latest react-router-dom v6.24.1 you have to use <Routes></Routes> instead of <Switch>
+2. If you want to show navbar at the top, create <route path="/" element={<Navbar />} ... </Route>
+    Between ... you can route your other components, like Home, About etc.
+3. When use <Link> tag instead of <a> tag, you have to use <Outlet /> method at the very end of the file and also import it, It comes from react-router-dom package.
+    Use-case - Mainly in Navbar, Footer like areas where we have mention other end-points.
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
