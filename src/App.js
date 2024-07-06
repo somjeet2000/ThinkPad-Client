8fafd38daf194ebff0761274499cb414bc0af4f0
Somjeet Srimani <somjeetsrimani2000@gmail.com>
2024-07-07 02:46:16 +0530

    Removed unused import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
-import Alert from './components/Alert';
 
 /*
 Very Important:-
